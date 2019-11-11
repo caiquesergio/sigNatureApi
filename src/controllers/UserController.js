611b6e8b6f9dea7b8e19b107a27efe6735f944f6
André Kitano <andre.kitano.silva@gmail.com>
2019-11-10 21:54:38 -0300

    Added comments for understanding

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,9 +1,12 @@
 const User = require('../models/User');
 
 module.exports = {
+    // Body = JSON usuario(andre, andre@gmail, masculino, 01011999)
+    // Query localhost:3333/users&nome=andre?email....
+    // header Usuario é autenticado e etc, bool, updated e etc
     async store(req, res) {
-        const { name, email, genre, birth} = req.body;
-
+        const { name, email, genre, birth } = req.body;
+        console.log(req)
         const user = await User.create({name, email, genre, birth});
 
         return res.json(user);
