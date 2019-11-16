f197ed5f1ef7ed24c24b9840586082dbd542854e
André Kitano <andre.kitano.silva@gmail.com>
2019-11-16 01:13:13 -0300

    Added docker file and env for aws deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
+const hostUrl = 'signature.co14q5vvollv.us-east-1.rds.amazonaws.com';
+
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    port: 54320,
-    username: 'unip',
-    password: 'unipeiros',
-    database: 'signature',
+    host: hostUrl,
+    port: 5432,
+    username: 'andrekitano',
+    password: '14719955lkloP',
+    database: 'signaturedb',
     define: {
         timestamps: true,
         underscored: true,
